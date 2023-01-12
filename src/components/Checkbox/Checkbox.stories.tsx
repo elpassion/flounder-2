fb95006b4b12e148065da9f262d7f8264abd81d8
piotrekzawada <p.zawada.94@gmail.com>
2023-01-12 16:00:23 +0100

    Add sample component's doc

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import {
+  Title,
+  Description,
+  Primary,
+} from '@storybook/addon-docs';
 
 import { Checkbox } from './';
 
@@ -9,11 +14,31 @@ export default {
   component: Checkbox,
 } as ComponentMeta<typeof Checkbox>;
 
-export const Primary: ComponentStory<typeof Checkbox> = () => <Checkbox />;
+export const Basic: ComponentStory<typeof Checkbox> = () => <Checkbox />;
+export const Secondary: ComponentStory<typeof Checkbox> = () => <Checkbox />;
 
-Primary.parameters = {
+Basic.parameters = {
   design: {
     type: "figma",
     url: "https://www.figma.com/file/xy6otn2JWHNdF70Tuq0UbS/TACO-Design-System-%5BDRAFT%5D?node-id=2054%3A3055&t=VjSme4oWsUXD1DSL-4"
+  },
+  docs: {
+    page: () => (
+      <>
+        <Title />
+        <Description />
+        <Primary />
+      </>
+    ),
+  }
+}
+
+Secondary.parameters = {
+  docs: {
+    page: () => (
+      <>
+        <h1>test doc 2 </h1>
+      </>
+    )
   }
 }
