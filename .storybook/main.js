c64d99d87a56a71599da2ab1e7969138fb3fe6ba
piotrekzawada <p.zawada.94@gmail.com>
2023-01-12 13:46:10 +0100

    Add a11y plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,8 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
     "storybook-addon-designs",
     "@storybook/preset-create-react-app"
   ],
