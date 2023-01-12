e1f83f0c92f249d3913da1d1bb458811c07af209
piotrekzawada <p.zawada.94@gmail.com>
2023-01-12 13:08:49 +0100

    Add designs addon

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -10,3 +10,10 @@ export default {
 } as ComponentMeta<typeof Checkbox>;
 
 export const Primary: ComponentStory<typeof Checkbox> = () => <Checkbox />;
+
+Primary.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/xy6otn2JWHNdF70Tuq0UbS/TACO-Design-System-%5BDRAFT%5D?node-id=2054%3A3055&t=VjSme4oWsUXD1DSL-4"
+  }
+}
