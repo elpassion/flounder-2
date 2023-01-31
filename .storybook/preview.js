d4521003b89386d26721d611e22ea4ec6e712077
piotrekzawada <p.zawada.94@gmail.com>
2023-01-31 14:40:18 +0100

    Add addon-actions config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,19 @@
 import "!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css";
 import "tailwindcss/tailwind.css";
-import { useDarkMode } from 'storybook-dark-mode'
-
+import { useDarkMode } from "storybook-dark-mode";
 
 export const parameters = {
-  actions: { disable: true },
+  actions: { argTypesRegex: "^on.*" },
 };
 
 export const decorators = [
   (Story) => {
-    const mode = useDarkMode() ? 'dark' : 'light';
+    const mode = useDarkMode() ? "dark" : "light";
 
     return (
       <div className={mode}>
         <Story />
       </div>
-    )
-  }
-]
+    );
+  },
+];
