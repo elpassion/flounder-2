cb304c9a69a8c9253f441362b0a433430c0058bb
Monika Zyznowska <monika.zyznowska@elpassion.pl>
2023-02-09 10:05:48 +0100

    Add separate active style for left anchor

--- src/components/Anchor/Anchor.stories.tsx
+++ src/components/Anchor/Anchor.stories.tsx
@@ -16,12 +16,15 @@ export const Anchor: ComponentStory<React.FC<AnchorProps>> = ({
     none: "text-primary-500 hover:text-primary-600 focus:text-primary-300",
     bottom:
       "border-b border-primary-500 text-primary-500 hover:text-primary-600 hover:border-primary-600 focus:text-primary-300 focus:border-primary-300",
-    left: "border-l-2 px-4 py-2 border-neutral-100 text-neutral-400 hover:border-neutral-400 focus:border-neutral-600 active:text-primary-500 active:border-primary-500",
+    left: "border-l-2 px-4 py-2 border-neutral-100 text-neutral-400 hover:border-neutral-400 focus:border-neutral-600",
   };
+
+  //TIP:classes to use for active tab link
+  const activeStyle = "text-primary-500 border-primary-500";
 
   return (
     <a
-      className={classNames("text-sm", decorationVariants[decoration])}
+      className={classNames("text-sm", decorationVariants[decoration], activeStyle)}
       href={link}
     >
       {text}
