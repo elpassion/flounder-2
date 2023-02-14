faec73f6aa50b3f9777f971c739bf22a7651331a
Michał Warda <michal.warda@elpassion.pl>
2023-02-14 21:18:33 +0100

    Try more complicated after generate script

--- examples/only-backend/afterGenerate.js
+++ examples/only-backend/afterGenerate.js
@@ -1,1 +1,9 @@
-console.log('After generation!');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Who are you?', (name) => {
+  console.log(`Hey there ${name}!`);
+  readline.close();
+});
