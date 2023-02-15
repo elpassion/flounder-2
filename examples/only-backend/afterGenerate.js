53cf9378530b8f67518a745d40d26a70ae245514
Michał Warda <michal.warda@elpassion.pl>
2023-02-15 11:45:26 +0100

    Add logs. Ignore node_modules

--- examples/only-backend/afterGenerate.js
+++ examples/only-backend/afterGenerate.js
@@ -18,14 +18,17 @@ const run = async () => {
   const clientNameRegex = /.*(elpassion).*#REPLACE_CLIENT/g;
   const projectNameRegex = /.*(fch).*#REPLACE_PROJECT/g;
 
+  console.log('Replacing project and client references...');
   replace.sync({
     files: '**/*',
+    ignore: 'node_modules/**/*',
     from: [clientNameRegex, projectNameRegex],
     to: [
       (match) => match.replaceAll('elpassion', clientName),
       (match) => match.replaceAll('fch', projectName),
     ],
   });
+  console.log('Successfully replaced project files!');
 };
 
 run();
