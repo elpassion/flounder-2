42d550fe4e166d11b75c4bcf4bf465e0c0dd865a
Michał Warda <michal.warda@elpassion.pl>
2023-02-15 10:44:37 +0100

    Change afterGenerate.js to include replacing results

--- examples/only-backend/afterGenerate.js
+++ examples/only-backend/afterGenerate.js
@@ -5,10 +5,28 @@ const readline = require('readline/promises').createInterface({
   output: process.stdout,
 });
 
-const projectName = await readline.question(
-  'Specify project name used on AWS:\n'
-);
+const run = async () => {
+  const projectName = await readline.question(
+    'Specify project name used on AWS:\n'
+  );
 
-const clientName = await readline.question(
-  'Specify client name used on AWS:\n'
-);
+  const clientName = await readline.question(
+    'Specify client name used on AWS:\n'
+  );
+
+  const clientNameRegex = /.*(elpassion).*#REPLACE_CLIENT/;
+  const projectNameRegex = /.*(fch).*#REPLACE_PROJECT/;
+
+  const results = replace.sync({
+    files: 'iac/**/*',
+    from: [clientNameRegex, projectNameRegex],
+    to: [
+      (match) => match.replaceAll('elpassion', clientName),
+      (match) => match.replaceAll('fch', projectName),
+    ],
+  });
+
+  console.log(results);
+};
+
+run();
