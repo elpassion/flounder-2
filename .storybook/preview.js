6200475a50de899168d644982977876ca0b96993
pieecia <piotr.kananowicz@elpassion.pl>
2023-02-22 10:25:46 +0100

    feat: ELP logo

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,28 @@
 import "!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css";
 import "tailwindcss/tailwind.css";
 import { useDarkMode } from "storybook-dark-mode";
+import { create } from "@storybook/theming";
+import logo from "./logo.svg";
+
+const sharedStyles = {
+  brandTitle: "El Passion",
+  brandUrl: "https://www.elpassion.com/",
+  brandImage: logo,
+  brandTarget: "_self",
+};
 
 export const parameters = {
   actions: { argTypesRegex: "^on.*" },
+  darkMode: {
+    dark: create({
+      ...sharedStyles,
+      base: "dark",
+    }),
+    light: create({
+      ...sharedStyles,
+      base: "light",
+    }),
+  },
 };
 
 export const decorators = [
