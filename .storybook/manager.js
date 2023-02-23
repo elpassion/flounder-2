b0f2e1132bb11d4bcc9cb1051c872cc0e55aa62f
pieecia <piotr.kananowicz@elpassion.pl>
2023-02-23 09:23:19 +0100

    light/dark mode

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,10 +1,14 @@
 import { addons } from "@storybook/addons";
+import React from "react";
+import { useDarkMode } from "storybook-dark-mode";
 
 addons.setConfig({
   sidebar: {
     renderLabel: ({ name, type }) => {
       if (type === "root") {
-        return `📚 ${name}`;
+        const mode = useDarkMode() ? "rgb(212 212 212)" : "rgb(163 163 163)";
+
+        return <span style={{ color: mode }}>{name}</span>;
       }
 
       return name;
