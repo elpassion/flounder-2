c8eeeb4ddadf61bdb1f97644bcf0fc210e67f160
pieecia <piotr.kananowicz@elpassion.pl>
2023-02-23 15:39:56 +0100

    feat: Badge

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,34 @@
+import classNames from "classnames";
+
+export interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
+  text: string;
+  size: "sm" | "md" | "lg";
+  variant: "filled" | "outline";
+}
+
+export const Badge: React.FC<BadgeProps> = ({ text, size, variant }) => {
+  const badgeSizeVariants = {
+    sm: "text-xs px-4 py-1",
+    md: "text-sm px-5 py-1.5",
+    lg: "text-base px-4 py-2.5",
+  };
+
+  const badgeStyleVariants = {
+    filled: "bg-neutral-50",
+    outline: "",
+  };
+
+  return (
+    <span
+      className={classNames(
+        "rounded border border-solid border-neutral-200 font-semibold text-neutral-500",
+        badgeSizeVariants[size],
+        badgeStyleVariants[variant]
+      )}
+    >
+      {text}
+    </span>
+  );
+};
+
+export default Badge;
---
+++ src/stories/atoms/Badge.stories.tsx
@@ -0,0 +1,36 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { BadgeProps } from "components/Badge";
+import { Badge as BadgeComponent } from "components/Badge";
+
+export const Badge: ComponentStory<React.FC<BadgeProps>> = ({ ...props }) => (
+  <BadgeComponent {...props} />
+);
+
+export default {
+  title: "Atoms/Badge",
+  component: Badge,
+  argTypes: {
+    text: {
+      description: "string",
+    },
+    size: {
+      control: "select",
+      options: ["sm", "md", "lg"],
+    },
+    variant: {
+      control: "select",
+      options: ["filled", "outline"],
+    },
+  },
+  args: {
+    text: "Badge",
+    size: "md",
+    variant: "filled",
+  },
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/xy6otn2JWHNdF70Tuq0UbS/TACO-Design-System-%5BDRAFT%5D?node-id=3608%3A22989&t=M2SScZpjMjPr5j2q-0",
+    },
+  },
+} as ComponentMeta<React.FC<BadgeProps>>;
