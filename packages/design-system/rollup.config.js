cf89d30823d5061297606fffd7eed46b27497588
Mariusz Heyda <insanepl@gmail.com>
2023-03-13 20:53:22 +0100

    Rollup pnp published package poc

--- packages/design-system/rollup.config.js
+++ packages/design-system/rollup.config.js
@@ -1,63 +1,36 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
-import json from '@rollup/plugin-json';
-const fs = require('fs');
-
-export const getFiles = (entry, extensions = [], excludeExtensions = []) => {
-  let fileNames = [];
-  const dirs = fs.readdirSync(entry);
-
-  dirs.forEach((dir) => {
-    const path = `${entry}/${dir}`;
-
-    if (fs.lstatSync(path).isDirectory()) {
-      fileNames = [
-        ...fileNames,
-        ...getFiles(path, extensions, excludeExtensions),
-      ];
-
-      return;
-    }
-
-    if (!excludeExtensions.some((exclude) => dir.endsWith(exclude))
-      && extensions.some((ext) => dir.endsWith(ext))
-    ) {
-      fileNames.push(path);
-    }
-  });
-
-  return fileNames;
-};
-
-const extensions = ['.js', '.ts', '.jsx', '.tsx'];
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./npm-package.json";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  input: [
-    './src/index.ts',
-    ...getFiles('./src/components', extensions),
-    ...getFiles('./src/helpers', extensions),
-    ...getFiles('./src/svgs', extensions),
-    ...getFiles('./src/utils', extensions),
+  input: "./src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true
+    }
   ],
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    preserveModules: true,
-    preserveModulesRoot: 'src',
-  },
   plugins: [
-    resolve(),
-    commonjs(),
-    json(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist',
+    peerDepsExternal({
+      includeDependencies: true
     }),
-    terser(),
-  ],
-  external: ['react', 'react-dom'],
+    // svgr(),
+    resolve(),
+    typescript({
+      exclude: ["**/__tests__/**"],
+      clean: true,
+    }),
+    commonjs(),
+    terser()
+  ]
 };
